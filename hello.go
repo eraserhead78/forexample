d246bf53bf5691308d69f9aa914096b43f0e1cbc
Shawn farley <eraserhead78>
2020-07-22 21:31:45 +0100

    changed

---
+++ hello.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello")
+
+	n := "Shawn"
+	fmt.Println(n)
+
+	n = "Farley"
+	fmt.Println(n)
+
+	n = "Jack"
+	fmt.Println(n)
+
+	n = "Jackson"
+	fmt.Println(n)
+}
